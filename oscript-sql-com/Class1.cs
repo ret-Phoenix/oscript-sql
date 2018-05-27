101e1d670a2fa53076883061ecba1e357629dc61
yury deshin <j.deshin@hotmail.com>
2018-05-27 18:00:22 +0300

    Отменил

---
+++ oscript-sql-com/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oscript_sql_com
+{
+    public class Class1
+    {
+    }
+}
